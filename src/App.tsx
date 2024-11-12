c900880ce382c8fe7f2bf17fe4ffd1dc0e1c510d
bobelmasry <alyhassan2254@gmail.com>
2024-11-12 12:30:28 +0200

    should avoid refreshing now

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ const supabaseUrl = 'https://nfexunixjcylaxsuasuc.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mZXh1bml4amN5bGF4c3Vhc3VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzNjcwNTAsImV4cCI6MjA0Njk0MzA1MH0.-ESP-AVbSh9JnH7ajPqTxVFxVeeFUZz_eSpteXEvMgA';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+
 const transformCoordinates = (coordinates : number[]) => {
   const transformed = [];
   for (let i = 0; i < coordinates.length; i += 2) {
@@ -18,6 +19,8 @@ const transformCoordinates = (coordinates : number[]) => {
 };
 
 function App() {
+  const [mapLoaded, setMapLoaded] = useState(false);
+
   const mapRef = useRef<mapboxgl.Map | undefined>(undefined);  // Explicitly set the type
   const mapContainerRef = useRef(null);
   const [geoData, setGeoData] = useState<FeatureCollection<Geometry>>({
@@ -106,38 +109,38 @@ function App() {
   }, []);
 
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYWx5eW91c3NlZiIsImEiOiJjbTNkaWJxNzMwM3poMm1xeTQ1cmFlZTVqIn0.vPHATw-9Rs7j3y-iW7oPJA';
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      center: [31.499159810172785, 30.017576505538145],
-      zoom: 16.12,
-    });
-
-    mapRef.current.on('load', () => {
-      mapRef.current.addSource('parking', {
-        type: 'geojson',
-        data: geoData,
+    if (!mapLoaded) {
+      mapboxgl.accessToken = 'pk.eyJ1IjoiYWx5eW91c3NlZiIsImEiOiJjbTNkaWJxNzMwM3poMm1xeTQ1cmFlZTVqIn0.vPHATw-9Rs7j3y-iW7oPJA';
+      mapRef.current = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        center: [31.499159810172785, 30.017576505538145],
+        zoom: 16.12,
       });
-
-      mapRef.current.addLayer({
-        id: 'parking-layer',
-        type: 'fill',
-        source: 'parking',
-        paint: {
-          'fill-color': [
-            'case',
-            ['==', ['get', 'status'], 'occupied'], '#FF0000',
-            '#32a852',
-          ],
-        },
+  
+      mapRef.current.on('load', () => {
+        mapRef.current.addSource('parking', {
+          type: 'geojson',
+          data: geoData,
+        });
+  
+        mapRef.current.addLayer({
+          id: 'parking-layer',
+          type: 'fill',
+          source: 'parking',
+          paint: {
+            'fill-color': [
+              'case',
+              ['==', ['get', 'status'], 'occupied'], '#FF0000',
+              '#32a852',
+            ],
+          },
+        });
+  
+        setMapLoaded(true);
       });
-    });
-
-    return () => {
-      mapRef.current.remove();
-    };
-  }, []);
-
+    }
+  }, [mapLoaded, geoData]);
+  
   useEffect(() => {
     if (mapRef.current && mapRef.current.isStyleLoaded()) {
       const source = mapRef.current.getSource('parking') as mapboxgl.GeoJSONSource;
